539105752422f5125c53d494089624e4b0ed023d
Ian Mayo <ian@planetmayo.com>
2025-08-07 15:24:04 +0100

    increment version

--- src/utils/version.js
+++ src/utils/version.js
@@ -4,7 +4,7 @@
  */
 
 // This value is replaced automatically by the generation script.
-export const VERSION = '0.1.2'
+export const VERSION = '0.1.4'
 
 /**
  * Get the current version of GramFrame
