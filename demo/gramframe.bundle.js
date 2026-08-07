61d7ce2f39ceea9a8e8bd03f471468afdf07709c
IanMayo <IanMayo@users.noreply.github.com>
2026-08-07 11:52:14 +0000

    deploy: 07811f205a08d644bc1dfe1ec0b87317c4ef0d60

--- demo/gramframe.bundle.js
+++ demo/gramframe.bundle.js
@@ -8,7 +8,7 @@ var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "sy
   document.head.appendChild(style);
 
   "use strict";
-  const VERSION = "0.1.15";
+  const VERSION = "0.1.16";
   function getVersion() {
     return VERSION;
   }
